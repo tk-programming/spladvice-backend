7dc1f1203169ce373fdbdbdea123a6efd5c00b04
tkyszk <qma912@yahoo.co.jp>
2024-05-29 00:04:09 +0900

    ステップ3を復活＆ポート番号を取得するように変更

--- app.py
+++ app.py
@@ -146,4 +146,4 @@ def delete_article():
 ########################################## main ##########################################
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
